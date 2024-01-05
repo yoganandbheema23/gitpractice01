d55f6679c2ad9d506be8e4f1005e758177f0b31e
sharun007 <sharunjames12@gmail.com>
2024-01-05 21:17:14 +0530

    hub007 branch is added

---
+++ src/test/java/employee/pack01.java
@@ -0,0 +1,15 @@
+package employee;
+
+public class pack01 {
+
+	public void printHello() {
+        System.out.println("Hello, world!");
+    }
+
+    // Method with parameters and return type
+    public int addNumbers(int a, int b) {
+        return a + b;
+    }
+
+   
+}
